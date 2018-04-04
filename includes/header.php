f1cb3c575766b1f3c188592d52ade592d99ff50d
Aung Myo Myint <aungtotetote19111995@gmail.com>
2018-04-04 09:34:30 +0630

    file

--- includes/header.php
+++ includes/header.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="images/favicon.ico">
+    <link rel="icon" href="images/favicon.png" >
 
-    <title>Blog Template for Bootstrap</title>
+    <title>⸎BlogL♥ver⸎</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
