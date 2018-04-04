b92e658c2575554bf5962c3da79a579c5c79031b
Aung Myo Myint <aungtotetote19111995@gmail.com>
2018-04-04 09:26:50 +0630

    minor

--- post.php
+++ post.php
@@ -1,50 +1,72 @@
 <?php
+include 'config/config.php';
+include 'libraries/Database.php';
+include 'helpers/format_helper.php';
 include 'includes/header.php';
 ?>
 
 
 
-            <div class="blog-post">
-                <h2 class="blog-post-title">Sample blog post</h2>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-
-                <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-                <hr>
-                <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-                <blockquote>
-                    <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                </blockquote>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <h2>Heading</h2>
-                <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-                <pre><code>Example code block</code></pre>
-                <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <h3>Sub-heading</h3>
-                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                <ul>
-                    <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                    <li>Donec id elit non mi porta gravida at eget metus.</li>
-                    <li>Nulla vitae elit libero, a pharetra augue.</li>
-                </ul>
-                <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-                <ol>
-                    <li>Vestibulum id ligula porta felis euismod semper.</li>
-                    <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                    <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-                </ol>
-                <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-
-            </div><!-- /.blog-post -->
-
-
-
-
-
-
-
 <?php
-include 'includes/footer.php';
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+
+    //Create DB Obj
+    $db = new Database();
+
+    //Create Query
+    $query = "Select * from posts where id=$id ";
+
+    //Run Query
+    $posts = $db->select($query);
+    $post = $posts->fetch_assoc();
+
+
+
+    //Create Query
+    $query = "Select * from categories";
+
+    //Run Query
+    $categories = $db->select($query);
+}
+
+
+
 ?>
+
+    <?php   if($post):   ?>
+
+
+        <div class="blog-post">
+            <h4 class="blog-post-title"><?php echo $post['title']; ?> </h4>
+
+            <p class="blog-post-meta">
+                <?php echo formatDate($post['date']); ?> by
+                <a href="#">
+                    <?php echo $post['author']; ?>
+                </a>
+            </p>
+
+            <?php echo $post['body']; ?>
+
+        </div><!-- /.blog-post -->
+
+    <?php   else :   ?>
+        <p>There is no post here.<p>
+    <?php   endif;   ?>
+
+
+
+
+
+
+
+
+
+
+
+    <?php
+        include 'includes/footer.php';
+    ?>
 
