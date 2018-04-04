e09320cb5bd6dc1166e2fee0851abbf0082ae398
Aung Myo Myint <aungtotetote19111995@gmail.com>
2018-04-04 07:29:05 +0630

    m

--- libraries/Database.php
+++ libraries/Database.php
@@ -39,4 +39,62 @@ class Database{
     }
 
 
+    /*
+     * Select
+    */
+    public function select($query){
+        $result = $this->link->query($query) or die($this->link->error._LINE_);
+        if($result->num_rows > 0){
+            return $result;
+        }else{
+            return false;
+        }
+    }
+
+    /*
+     * Insert
+     */
+    public function insert($query){
+        $insert_row = $this->link->query($query) or die($this->link->error._LINE_);
+
+        //Validate
+        if($insert_row){
+            header("Location: index.php?msg=".urlencode('Record Added'));
+        }else{
+            die('Error: ('.$this->link->errno .')'.$this->link->error);
+        }
+    }
+
+
+
+    /*
+     * Update
+     */
+    public function update($query){
+        $update_row = $this->link->query($query) or die($this->link->error._LINE_);
+
+        //Validate
+        if($update_row){
+            header("Location: index.php?msg=".urlencode('Record Updateed'));
+        }else{
+            die('Error: ('.$this->link->errno .')'.$this->link->error);
+        }
+    }
+
+
+    /*
+     * Delete
+     */
+    public function delete($query){
+        $delete_row = $this->link->query($query) or die($this->link->error._LINE_);
+
+        //Validate
+        if($delete_row){
+            header("Location: index.php?msg=".urlencode('Record Deleted'));
+        }else{
+            die('Error: ('.$this->link->errno .')'.$this->link->error);
+        }
+    }
+
+
 }
